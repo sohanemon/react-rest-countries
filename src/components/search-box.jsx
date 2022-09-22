64d0e53dc4d8288c7c4201dcd65e7f4b2e2294c0
sohanemon <m.sohanemon@gmail.com>
2022-09-22 22:32:51 +0600

    buggy

--- src/components/search-box.jsx
+++ src/components/search-box.jsx
@@ -24,10 +24,7 @@ function SearchBox() {
           <div className='relative w-full overflow-hidden text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm'>
             <Combobox.Input
               className='w-full py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 border-none focus:ring-0'
-<<<<<<< HEAD
               displayValue={(country) => country?.name?.common || "search"}
-=======
->>>>>>> parent of 61d7af8 (country name added)
               onChange={(event) => setQuery(event.target.value)}
             />{" "}
             <Combobox.Button className='absolute inset-y-0 right-0 flex items-center pr-2'>
