0b551a84cf6a3913dc231f94f853c36432f8f333
sohanemon <m.sohanemon@gmail.com>
2022-09-22 22:38:16 +0600

    going back

--- src/components/search-box.jsx
+++ src/components/search-box.jsx
@@ -40,14 +40,6 @@ function SearchBox() {
                 </Combobox.Option>
               ))}
             </Combobox.Options> */}
-          </div>
-          <Transition
-            as={Fragment}
-            leave='transition ease-in duration-100'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
-            afterLeave={() => setQuery("")}
-          >
             <Combobox.Options className='absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
               {filteredCountries.length === 0 && query !== "" ? (
                 <div className='relative px-4 py-2 text-gray-700 cursor-default select-none'>
@@ -93,7 +85,7 @@ function SearchBox() {
                 ))
               )}
             </Combobox.Options>
-          </Transition>
+          </div>
         </div>
       </Combobox>
     </div>
